c3ada0dde8e3f6c36f5ac3e17da7c35366748676
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-06-27 21:06:27 +0530

    Create CollectionsTreeSet1.java

---
+++ CollectionsTreeSet1.java
@@ -0,0 +1,22 @@
+import java.util.TreeSet;
+import java.util.Iterator;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        TreeSet<String> set = new TreeSet<>();
+        set.add("One");
+        set.add("Two");
+        set.add("Three");
+
+        System.out.println("Using for-each loop:");
+        for (String element : set) {
+            System.out.println(element);
+        }
+
+        System.out.println("Using Iterator:");
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
