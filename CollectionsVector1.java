757748c1b899711b45d7f7925af848f5873d7a94
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-06-27 21:00:09 +0530

    Create CollectionsVector1.java

---
+++ CollectionsVector1.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+import java.util.Iterator;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Vector<String> list = new Vector<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+
+        System.out.println("Using for-each loop:");
+        for (String fruit : list) {
+            System.out.println(fruit);
+        }
+
+        System.out.println("Using Iterator:");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
