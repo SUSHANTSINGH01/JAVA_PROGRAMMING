2f6a0220bc94613314ff8fc2a977a6c1457cbf5e
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-06-27 19:57:37 +0530

    Create CollectionsLinkedList1.java

---
+++ CollectionsLinkedList1.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+
+        System.out.println("Using for-each loop:");
+        for (String fruit : list) {
+            System.out.println(fruit);
+        }
+
+        System.out.println("Using Iterator:");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
