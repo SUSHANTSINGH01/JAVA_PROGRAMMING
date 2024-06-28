2c3cf515830e9f63b99b0f6eda630ecefb854155
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-06-28 18:37:28 +0530

    Update CollectionsHashSet1.java

--- CollectionsHashSet1.java
+++ CollectionsHashSet1.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Iterator;
 
-public class ArrayListExample {
+public class HashSetExample {
     public static void main(String[] args) {
         HashSet<String> set = new HashSet<>();
         set.add("One");
