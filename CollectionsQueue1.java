3b0005da72ab35ff52031fa586cdbefb1ba6aacf
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-06-28 19:01:22 +0530

    Create CollectionsQueue1.java

---
+++ CollectionsQueue1.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreeMapExample {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>(); // can implement only with LinkedList, can't implement with ArrayList and vector
+        queue.add("First");
+        queue.add("Second");
+        queue.add("Third");
+
+        System.out.println("Queue elements:");
+        while (!queue.isEmpty()) {
+            System.out.println(queue.remove());
+        }
+    }
+}
